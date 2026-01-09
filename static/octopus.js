78530c39e138bd2690451d604bd0f15fec76d74e
Alan Lai <alanlai22@outlook.com>
2026-01-09 14:09:53 +0800

    octopus init

--- static/octopus.js
+++ static/octopus.js
@@ -1,24 +1,26 @@
 const ndef = new NDEFReader();
 
-
 document.addEventListener("DOMContentLoaded", function() {
-    ndef
-        .scan()
-        .then(() => {
-            console.log("Scan started successfully.");
-                ndef.onreadingerror = (event) => {
-                    document.getElementById('error').innerText = "Error! Cannot read data from the NFC tag. Try a different one?"
-                console.log(
-                    "Error! Cannot read data from the NFC tag. Try a different one?",
-                );
-            };
-            ndef.addEventListener("reading" , ({ message, serialNumber }) => {
-                document.getElementById('error').innerText = "NDEF message read."
-                console.log("NDEF message read.");
-            });
-        })
-        .catch((error) => {
-            document.getElementById('error').innerText = `Error! Scan failed to start: ${error}.`
-            console.log(`Error! Scan failed to start: ${error}.`);
-    });
+    let scanButton = document.getElementById("scan")
+    scanButton.addEventListener("click", () => {
+        ndef
+            .scan()
+            .then(() => {
+                console.log("Scan started successfully.");
+                    ndef.onreadingerror = (event) => {
+                        document.getElementById('error').innerText = "Error! Cannot read data from the NFC tag. Try a different one?"
+                    console.log(
+                        "Error! Cannot read data from the NFC tag. Try a different one?",
+                    );
+                };
+                ndef.addEventListener("reading" , ({ message, serialNumber }) => {
+                    document.getElementById('error').innerText = "NDEF message read."
+                    console.log("NDEF message read.");
+                });
+            })
+            .catch((error) => {
+                document.getElementById('error').innerText = `Error! Scan failed to start: ${error}.`
+                console.log(`Error! Scan failed to start: ${error}.`);
+        });
+    })
 })
